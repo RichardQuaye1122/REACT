5136c06d7f853cec06cf8b9c3ff8af34b7f6bda5
RichardQuaye1122 <quayerichard1122@gmail.com>
2025-10-29 15:35:36 +0000

    first commit

---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-orange-500 place-self-center py-20 px-20'>
+        <h1 className='text-xl font-bold hover:underline hover:text-gray-500 transform tra'>Hello, Welcome to Mr. Quaye's Yard</h1>
+        <p className='text-sm'>Here at Mr. Quaye's Yard, We serve you the best to your sactisfaction</p>
+        <button className='shadow-lg round-lg bg-green-300 py-2 px-2'>Get Started</button>
+    </div>
+  )
+}
+
+export default Home
