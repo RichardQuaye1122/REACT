ff0b4a0ace03185b41f2313a274760c8ea8a4322
RichardQuaye1122 <quayerichard1122@gmail.com>
2025-11-20 13:20:59 +0000

    week 1 exercise

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-orange-500 place-self-center py-20 px-20'>
+        <h1 className='text-xl font-bold hover:underline hover:text-gray-500 transform tra'>Hello, Welcome to Mr. Quaye's Yard</h1>
+        <p className='text-sm'>Here at Mr. Quaye's Yard, We serve you the best to your sactisfaction</p>
+        <button className='shadow-lg round-lg bg-green-300 py-2 px-2'>Get Started</button>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel architecto molestiae repellendus consequatur necessitatibus unde molestias laborum incidunt, exercitationem fugiat dolorum dolores amet autem perferendis, modi laudantium quia quos quaerat.</p>
+    </div>
+  )
+}
+
+export default Home
